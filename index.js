990734d5e12c16c74b9dde1cac7d55be3809cff3
irisjitomo <irisjitomo@eyahoo.com>
2019-09-30 17:33:03 -0400

    added errors

--- index.js
+++ index.js
@@ -1,97 +1,100 @@
 // implement your API here
 const express = require('express');
-const userData = require('./data/db')
-
+const userData = require('./data/db');
 
 const server = express();
 
 server.use(express.json());
 
-
 /////// do CRUD requests here
 
 server.get('/', (req, res) => {
-    res.json('hello testing testing')
-})
+	res.json('hello testing testing');
+});
 
 server.get('/testing', (req, res) => {
-    res.json('testing again')
-})
+	res.json('testing again');
+});
 
 // GET REQUEST for users
 server.get('/api/users', (req, res) => {
-    userData
-    .find()
-    .then(user => {
-        res.send(user)
-    })
-    .catch(err => {
-        console.log(err)
-    })
-})
+	userData
+		.find()
+		.then((user) => {
+			res.send(user);
+		})
+		.catch((err) => {
+			res.status(500).json({ error: 'The users information could not be retrieved.' });
+		});
+});
 
 // GET REQUEST for certain user :id
 server.get('/api/users/:id', (req, res) => {
-    const id = req.params.id
+    const id = req.params.id;
+        userData
+        .findById(id)
+        .then(user => {
+            if (user) {
 
-    userData
-    .findById(id)
-    .then(user => {
-        res.json(user)
-    })
-    .catch(err => {
-        console.log(err)
-    })
-})
+                res.json(user)
+                
+        .catch(() => {
+            res.status(500).json({ error: "The user information could not be retrieved."})
+        })
+            } else  {
+                res.status(404).json({ message: "The user with the specified ID does not exist."})
+            }
+        })
+});
 
 // POST REQUEST for adding user
 server.post('/api/users', (req, res) => {
-    const newUser = req.body
+	const newUser = req.body;
 
-    if (!newUser.name || !newUser.bio) {
-        res.status(400).json({ message: 'needs a complete data set'})
-    } else {
-        userData
-        .insert(newUser)
-        .then(user => {
-            res.json(user)
-        })
-        .catch(err => {
-            res.status(500).json({ error: "There was an error while saving the user to the database" })
-        })
-    }
-})
+	if (!newUser.name || !newUser.bio) {
+		res.status(400).json({ errorMessage: 'Please provide name and bio for the user.' });
+	} else {
+		userData
+			.insert(newUser)
+			.then((user) => {
+				res.status(201);
+				res.json(user);
+			})
+			.catch((err) => {
+				res.status(500).json({ error: 'There was an error while saving the user to the database' });
+			});
+	}
+});
 
 // DELETE REQUEST for deleting a specific id
 server.delete('/api/users/:id', (req, res) => {
-    const id = req.params.id
-    userData
-    .remove(id)
-    .then(user => {
-        res.json(user)
-    })
-    .catch(err => {
-        console.log(err)
-    })
-})
+	const id = req.params.id;
+	userData
+		.remove(id)
+		.then((user) => {
+			res.json(user);
+		})
+		.catch(() => {
+			res.status(500).json({ error: "The user could not be removed" })
+		});
+});
 
 // PUT REQUEST for updating a specific id
 server.put('/api/users/:id', (req, res) => {
-    const id = req.params.id 
-    const updatedUser = req.body;
+	const id = req.params.id;
+	const updatedUser = req.body;
 
-    userData
-    .update(id, updatedUser)
-    .then(user => {
-        res.json(user)
-    })
-    .catch(err => {
-        res.json(err)
-    })
-})
-
+	userData
+		.update(id, updatedUser)
+		.then((user) => {
+			res.json(user);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
+});
 
 ////// after CRUD requests
 
 const port = 8000;
-server.listen(port, () => console.log(`listening on port ${port}`))
+server.listen(port, () => console.log(`listening on port ${port}`));
