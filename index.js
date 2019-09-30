92baed331aa4ffad3bdd018697a8ee7b83df8e13
irisjitomo <irisjitomo@eyahoo.com>
2019-09-30 17:47:53 -0400

    all errors and statuses done

--- index.js
+++ index.js
@@ -68,30 +68,53 @@ server.post('/api/users', (req, res) => {
 
 // DELETE REQUEST for deleting a specific id
 server.delete('/api/users/:id', (req, res) => {
-	const id = req.params.id;
+    const id = req.params.id;
+    
 	userData
 		.remove(id)
-		.then((user) => {
-			res.json(user);
-		})
-		.catch(() => {
-			res.status(500).json({ error: "The user could not be removed" })
-		});
+		.then(user => {
+            if (user) {
+                res.json(user)
+            .catch(() => {
+                res.status(500).json({error: "The user could not be removed"})
+            })
+            } else {
+                res.status(404).json({message: "The user with the specified ID does not exist."})
+            }
+        })
 });
 
 // PUT REQUEST for updating a specific id
 server.put('/api/users/:id', (req, res) => {
 	const id = req.params.id;
-	const updatedUser = req.body;
+    const updatedUser = req.body;
+    
+    if (!updatedUser.name || !updatedUser.bio) {
+        res.status(400).json({errorMessage: "Please provide name and bio for the user."})
+    } else {
+        userData
+        .update(id, updatedUser)
+        .then((user) => {
+            if (user) {
+                res.json(user)
+                res.status(200)
+            .catch(() => {
+                res.status(500).json({ error: "The user information could not be modified." })
+            })
+            } else {
+                res.status(404).json({message: "The user with the specified ID does not exist."})
+            }
+        })
+    }
 
-	userData
-		.update(id, updatedUser)
-		.then((user) => {
-			res.json(user);
-		})
-		.catch((err) => {
-			res.json(err);
-		});
+	// userData
+	// 	.update(id, updatedUser)
+	// 	.then((user) => {
+	// 		res.json(user);
+	// 	})
+	// 	.catch((err) => {
+	// 		res.json(err);
+	// 	});
 });
 
 ////// after CRUD requests
