f81efc46f6cf2e3a23e993c1178f2e47d0e6b9ec
irisjitomo <irisjitomo@eyahoo.com>
2019-09-30 15:25:28 -0400

    made test get requests, and get request for users

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // implement your API here
+const express = require('express');
+const userData = require('./data/db')
+
+
+const server = express();
+
+server.use(express.json());
+
+
+/////// do CRUD requests here
+
+server.get('/', (req, res) => {
+    res.json('hello testing testing')
+})
+
+server.get('/testing', (req, res) => {
+    res.json('testing again')
+})
+
+server.get('/api/users', (req, res) => {
+    userData
+    .find()
+    .then(user => {
+        res.send(user)
+    })
+    .catch(err => {
+        console.log(err)
+    })
+})
+
+
+////// after CRUD requests
+
+const port = 8000;
+server.listen(port, () => console.log(`listening on port ${port}`))
